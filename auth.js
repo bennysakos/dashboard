71914930d009bb5e86f44417bc95672da4d8d86e
bennysakos <alodapos6@gmail.com>
2025-07-20 19:29:01 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,22 +2,23 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
-// OAuth login route
+// Redirect user to Discord login
 router.get('/login', passport.authenticate('discord'));
 
-// Callback route
+// Discord OAuth2 callback URL
 router.get('/callback', passport.authenticate('discord', {
   failureRedirect: '/'
 }), (req, res) => {
+  // Successful login, redirect to dashboard or homepage
   res.redirect('/dashboard');
 });
 
 // Logout route
-router.get('/logout', (req, res) => {
-  req.logout(() => {
+router.get('/logout', (req, res, next) => {
+  req.logout(function(err) {
+    if (err) { return next(err); }
     res.redirect('/');
   });
 });
 
 module.exports = router;
-
